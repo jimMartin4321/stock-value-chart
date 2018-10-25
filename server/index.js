cf9141fea813e38ea6fc1b54a75076d71ce34b2c
jimMartin4321 <jamesrm4321@gmail.com>
2018-10-25 12:31:27 -0700

    initialize app and server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var db = require('../db/database.js');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+let port = 3004;
+
+app.listen(port, function(err) {
+  if (err) {
+    return console.log(`error connection to port ${port}`);
+  }
+  console.log(`listening at port ${port}`);
+});
