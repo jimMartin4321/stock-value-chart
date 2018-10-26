c0fe1a2d7f97a1724679f2f8fe954ce151fed917
jimMartin4321 <jamesrm4321@gmail.com>
2018-10-26 12:49:20 -0700

    beggining to develop graph code

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-
+import Graph from './graph.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  getData() {
+    
   }
 
   render() {
     return (
       <div>
-        hello
+        <Graph data={data}/>
       </div>
     );
   }
