649f517686d5f89af2c52516a39ace90a6b09753
jimMartin4321 <jamesrm4321@gmail.com>
2018-10-29 16:32:21 -0700

    inserted dashed line to graph denoting the opening price

--- client/src/components/graph.jsx
+++ client/src/components/graph.jsx
@@ -5,18 +5,26 @@ class Graph extends React.Component {
     super(props);
   }
 
-  definePricePoints() {
+  pricePoints() {
     let line = '';
     let xPos = 0;
-    const yAxisMidpoint = (document.getElementById('App').clientHeight * 6/10) / 2;
+    const yAxisMidPoint = (document.getElementById('App').clientHeight * 6/10) / 2;
     const priceOffSet = this.props.data[0].price;
     this.props.data.forEach(price => {
-      const yPos = yAxisMidpoint + ((priceOffSet - price.price) * 4);
+      const yPos = yAxisMidPoint + ((priceOffSet - price.price) * 4);
       line += `${xPos},${yPos} `;
       xPos += 8;
     });
     return line;
   }
+
+  middleLine() {
+    const yAxisMidPoint = (document.getElementById('App').clientHeight * 6 / 10) / 2;
+    const xAxisEndPoint = document.getElementById('App').clientWidth;
+    return `0,${yAxisMidPoint} ${xAxisEndPoint},${yAxisMidPoint}`;
+  }
+
+  
 
   graphCreation() {
     if (this.props.data.length) {
@@ -25,8 +33,17 @@ class Graph extends React.Component {
           <polyline
             fill="none"
             stroke="#0074d9"
-            strokeWidth="1.25"
-            points={this.definePricePoints()} />
+            strokeWidth="2"
+            points={this.pricePoints()} 
+          />
+          <polyline
+            fill="none"
+            stroke="#D3D3D3"
+            strokeWidth="3"
+            strokeDasharray="1, 10"
+            strokeLinecap="round"
+            points={this.middleLine()}
+          />
         </svg>
       );
     }
