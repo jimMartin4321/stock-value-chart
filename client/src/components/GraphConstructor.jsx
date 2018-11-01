a747899400281a43bb70a9aab4e0e4aa99369dc1
jimMartin4321 <jamesrm4321@gmail.com>
2018-11-01 12:28:17 -0700

    refactored graph constructor to comply with airbnb styling

--- client/src/components/GraphConstructor.jsx
+++ client/src/components/GraphConstructor.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import { LineChart, Line, ResponsiveContainer, YAxis, XAxis, ReferenceLine, Tooltip } from 'recharts';
-import CustomToolTip from './CustomToolTip.jsx';
+import {
+  LineChart, Line, ResponsiveContainer, YAxis, XAxis, ReferenceLine, Tooltip,
+} from 'recharts';
+import CustomToolTip from './CustomToolTip';
 import styles from '../styles/GraphConstructor.css';
 
-const GraphConstructor = (props) => (
+const GraphConstructor = (props) => {
+  const { data } = props;
+  const { openingPrice } = props;
+  return (
+    <ResponsiveContainer className={styles.chart} width="100%" height="100%">
 
-  <ResponsiveContainer className={styles.chart} width='100%' height='100%'>
+      <LineChart onMouseMove={e => props.priceHover(e)} onMouseLeave={() => props.priceReset()} className="chart" width={document.getElementById('App').clientWidth} height={document.getElementById('App').clientHeight * 6 / 10}>
 
-    <LineChart onMouseMove={(e) => {props.priceHover(e) }} onMouseLeave={() => {props.priceReset()}} className="chart" width={document.getElementById('App').clientWidth} height={document.getElementById('App').clientHeight * 6 / 10} >
+        <YAxis type="number" domain={['dataMin', 'dataMax']} hide />
+        <XAxis dataKey="id" type="number" domain={[0, 78]} hide />
+        <Tooltip content={<CustomToolTip />} />
+        <ReferenceLine y={openingPrice} stroke="black" strokeDasharray="1 8" />
+        <Line type="monotone" dataKey="price" data={data} width={1} stroke="#30CD9A" dot={false} />
 
-      <YAxis type="number" domain={['dataMin', 'dataMax']} hide={true} />
-      <XAxis dataKey="id" type="number" domain={[0, 78]} hide={true} />
-      <Tooltip content={<CustomToolTip/>}/>
-      <ReferenceLine y={props.openingPrice} stroke="black" strokeDasharray="1 8"/>
-      <Line type="monotone" dataKey="price" data={props.data} width={1} stroke="#30CD9A" dot={false}/>
+      </LineChart>
 
-    </LineChart>
-
-  </ResponsiveContainer>
-
-);
+    </ResponsiveContainer>
+  );
+};
 
 export default GraphConstructor;
